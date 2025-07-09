aff0e331d1fc4aaaa160b81c33e1a6e2b3e168cd
hardikbatwal1505@gmail.com <hardikbatwal1505@gmail.com>
2025-07-09 23:53:50 +0530

    Upto PracticeSet 12

--- Practice_Set12/script.js
+++ Practice_Set12/script.js
@@ -1,16 +1,21 @@
-const a = async(text)=>{
-    return new Promise((resolve, reject)=>{
-        setTimeout(()=>{
-            resolve(text)
-        },2000)
-    })
-}
+//Q1 
+//Print the following words Hello and World each after a delay of 4sec
+// const a = async(text)=>{
+//     return new Promise((resolve, reject)=>{
+//         setTimeout(()=>{
+//             resolve(text)
+//         },2000)
+//     })
+// }
 
-(
-    async()=>{
-        let text=await a("Hello")
-        console.log(text)
-        let text2 =await a("world")
-        console.log(text2)
-    }
-)()
+// (
+//     async()=>{
+//         let text=await a("Hello")
+//         console.log(text)
+//         let text2 =await a("world")
+//         console.log(text2)
+//     }
+// )()
+
+//Q2
+//Find avg of number of an array using spread operation
