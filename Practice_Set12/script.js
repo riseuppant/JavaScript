18274ac02e90591e48e11ffd50e3c3331ce153f3
hardikbatwal1505@gmail.com <hardikbatwal1505@gmail.com>
2025-07-10 01:03:54 +0530

    Done PracticeSet 12

--- Practice_Set12/script.js
+++ Practice_Set12/script.js
@@ -19,3 +19,33 @@
 
 //Q2
 //Find avg of number of an array using spread operation
+// let arr1=[1,2,3]
+// function avg (a1,a2,a3){
+//     return (a1+a2+a3)/3;
+// }
+// console.log(avg(...arr1))
+
+//Q3
+// const a = async(text,n)=>{
+//     return new Promise((resolve, reject)=>{
+//         setTimeout(()=>{
+//             resolve(text)
+//         },2000*n)
+//     })
+// }
+
+// (
+//     async()=>{
+//         let text=await a("Hello",4)
+//         console.log(text)
+//         let text2 =await a("world",1)
+//         console.log(text2)
+//     }
+// )()
+
+//Q4
+function simple(p,r,t){
+    return (p*r*t)/100
+}
+
+console.log(simple(1000,1,10))
