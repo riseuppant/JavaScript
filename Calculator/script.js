ed1b04cefee7b54123e63cae2ad6f1504fa28e0d
riseuppant <hardikbatwal1505@gmail.com>
2025-07-22 13:26:46 +0530

    CSS Added

--- Calculator/script.js
+++ Calculator/script.js
@@ -3,3 +3,30 @@ console.log(display.value)
 const appendToDisplay =(char)=>{
     display.value+=char
 }
+
+let clearOne=()=>{
+    console.log("button working")
+    if(display.value!=''){
+        display.value=display.value.slice(0,display.value.length-1)
+        console.log(display.value.length)
+    }
+    
+    else{
+        display.value="ERROR"
+    }
+}
+
+let clearDisplay=()=>{
+    console.log("Log cleared");
+    if(display.value!='')
+    display.value=""
+    else{
+        display.value="LOG ALREADY CLEARED"
+    }
+}
+
+let toPercentage=()=>{
+    let a=(Number.parseFloat(display.value))*(0.01)
+    display.value=`${a}`
+}
+
