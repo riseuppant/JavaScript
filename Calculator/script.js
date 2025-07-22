21ebc652e265f93cb93489ce9463074fdb050a36
riseuppant <hardikbatwal1505@gmail.com>
2025-07-22 14:30:26 +0530

    Done with History

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,5 +1,8 @@
 let display= document.getElementById("display")
 let sol=document.getElementById("solnReal")
+let historyContainer=document.getElementById("historyContainer")
+
+
 console.log(display.value)
 const appendToDisplay =(char)=>{
     display.value+=char
@@ -30,9 +33,31 @@ let toPercentage=()=>{
     let a=(Number.parseFloat(display.value))*(0.01)
     display.value=`${a}`
 }
-
-let calculate=()=>{
+let setName=async(backDisp,historyBox)=>{
+    if(display.value===backDisp){
+        historyBox.innerHTML=display.value
+    }
+    else{
+        historyBox.innerHTML=backDisp+"="+display.value
+    }
+}
+let calculate=async()=>{
+    let backDisp=display.value
     display.value=eval(display.value)
+    
+    let historyBox=document.createElement("button")
+    
+    await setName(backDisp , historyBox)
+    
+    historyBox.addEventListener('click',()=>{
+        
+        let pos=historyBox.textContent.indexOf('=')
+        if(pos>0)
+            display.value=(historyBox.innerHTML.slice(0,pos))
+        else{
+            display.value=historyBox.innerHTML
+        }
+    })
+    historyContainer.appendChild(historyBox)
 }
 
-let
